9d4f7787e373ad90b5c6ac089c7f114b3a458a85
jakvah <jakobvahlin@gmail.com>
2020-08-04 23:24:06 +0000

    Add support for reading PDFs on webpage

--- __init__.py
+++ __init__.py
@@ -1,8 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,Markup
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/pdf_viewer/<filename>")
+def show_pdf(filename):    
+    try:
+        """
+        html_string = '<iframe src="' + "{" + "{" +'url_for' + "('static',filename='pdfs/" + str(filename) +".pdf')" + "}"+ '}"' + ' width="100%" height="100%">You browswer doesnt support PDFs. Consider downloading the PDF </iframe>'
+        tag = Markup(html_string)
+        """
+        location = "pdfs/" + str(filename) + ".pdf"
+        return render_template("pdf_viewer.html",link = location)
+    except Exception as e:
+        return str(e)
+    
 if __name__ == "__main__":
     app.run()
