9044e018740b41d9842d8dda5151d490d7495dd1
jakvah <jakobvahlin@gmail.com>
2020-12-02 17:21:05 +0000

    Complete add match procedure

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from database_management import *
+from database_management import db_interaction, database_exceptions
 from match_extraction import popflash_scraper as ps
 
 
@@ -7,5 +7,10 @@ def main():
     pop_match = ps.get_match_data(pop_id)
     print(pop_match.team_1)
     print(pop_match.team_2)
+
+    print("Adding match to database")
+    conn = db_interaction.get_database_connection()
+    db_interaction.add_match_data(conn,pop_match)
+    print("Done")
 
 main()
