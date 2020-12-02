9df4b8bc8d098843f1dda86d275470c5feb15668
jakvah <jakobvahlin@gmail.com>
2020-12-02 02:00:12 +0000

    Add outcome class

---
+++ match_extraction/Outcome.py
@@ -0,0 +1,13 @@
+"Outcome class"
+
+class Outcome:
+    def __init__(self):
+        pass
+
+class Tie(Outcome):
+    def __init__(self):
+        pass
+
+class Victory(Outcome):
+    def __init__(self):
+        pass
