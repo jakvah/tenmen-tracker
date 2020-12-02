6e78a0bbef8bd09a5eba36acf0bd60aac2fc6fa2
jakvah <jakobvahlin@gmail.com>
2020-12-02 02:08:16 +0000

    Add match outcome to Match

--- match_extraction/Match.py
+++ match_extraction/Match.py
@@ -1,4 +1,6 @@
 "Match class"
+from Team import Team
+from Outcome import Tie, Determined
 
 class Match:
     def __init__(self,match_id,*,team1=None,team2=None):
@@ -6,5 +8,13 @@ class Match:
         self.team_1 = team1
         self.team_2 = team2
 
+        self.match_outcome = determine_outcome
         
+    def determine_outcome(self,team1,team2):
+        if team1.get_score() > team2.get_score():
+            return Determined(victor=team1,loser=team2)
+        elif team1.get_score() < team2.get_score():
+            return Determined(victor=team2,loser=team1)
+        else:
+            return Tie()
         
